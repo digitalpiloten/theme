c88688e357435a93c7031d4420a5eb5880698fb2
MagicMarvMan <marvin.kai@gmx.de>
2019-11-27 17:21:48 +0100

    Germanize strings

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -21,13 +21,13 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p>
-				<?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'suki' ); ?>
+				<?php esc_html_e( 'Entschuldigung, es konnten keine Posts mit diesen Parametern gefunden werden. Bitte versuchen Sie es mit anderen Keywords erneut.', 'suki' ); ?>
 			</p>
 
 		<?php else : ?>
 
 			<p>
-				<?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'suki' ); ?>
+				<?php esc_html_e( 'Es sieht so aus, als könnten wir die gesuchten Inhalte nicht finden. Bitte versuchen sie das Nutzen der Suchfunktion.', 'suki' ); ?>
 			</p>
 
 			<?php get_search_form(); ?>
